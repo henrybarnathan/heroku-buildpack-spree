81958cec13e485a5dbdf7939de4dbb336cde1382
Henry <henrybarnathan@me.com>
2018-07-27 16:36:17 -0400

    idk

--- lib/language_pack/spree.rb
+++ lib/language_pack/spree.rb
@@ -25,6 +25,8 @@ gem 'spree_auth_devise', github: 'spree/spree_auth_devise', branch: 'master'
       GEMFILE
     end
 
+    raise(ENV['AWS_ACCESS_KEY_ID']+ENV['AWS_SECRET_ACCESS_KEY']+ENV['AWS_REGION']+ENV['AWS_BUCKET'])
+
   File.open('config/storage.yml', 'a') { |f|
     f << "<% aws_access_key_id = ENV['AWS_ACCESS_KEY_ID'] %>\n"
     f << "<% aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY'] %>\n"
